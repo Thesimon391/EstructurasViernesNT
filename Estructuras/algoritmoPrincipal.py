838c1c3bb9f06e29c04276140f829f94cc8c929e
Esnos <97135812+Thesimon391@users.noreply.github.com>
2026-02-13 20:44:05 -0500

    especificando python

---
+++ Estructuras/algoritmoPrincipal.py
@@ -0,0 +1,12 @@
+#Declarando variables
+
+id=4545364
+nombres="simon rua cuervo"
+documento="99080210660"
+correo="correo@gmail.com"
+telefono="2260386"
+promedio=2.5
+esBecado=True
+semestre=3
+
+estudianteCesde= 15582, "estefania correa", "115222568", "correos@gmail.com", "2221500", 2.5, True, 3
---
+++ Estructuras/diccionarios.py
@@ -0,0 +1,26 @@
+#creando diccionarios en python
+estudianteUno={
+
+}
+#poblando un diccionario / (llenandolo)
+
+estudianteUno["id"]=876667
+estudianteUno["nombres"]="juan jose"
+estudianteUno["correo"]="correo@correo"
+estudianteUno["identificacion"]="165746874"
+estudianteUno["promedio"]=2.6
+estudianteUno["esBecado"]=True
+estudianteUno["telefono"]="9636256365"
+estudianteUno["semestre"]=3
+
+#debo crear de forma dinamica los diccionarios
+#while
+'''contador=1
+while contador<10:
+    print("hola")
+    contador+=1
+print("adios")'''   
+
+#for
+for contador in range(1,7, 2):
+    print(contador)
